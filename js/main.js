3d3eeef8cb51c47e80bc5abf83ec224e053c5548
ZinedineMess <zinedinemessahel@hotmail.com>
2021-03-17 19:10:17 +0100

    feat: avancé sur la fonction compteur de like

--- js/main.js
+++ js/main.js
@@ -5,18 +5,20 @@
 import {} from './home/scrollButton.js';
 import {} from './home/filter.js';
 
-
 // PHOTOGRAPHERS PAGES
 import PhotographerProfil from './photographers-page/PhotographerProfil.js';
 import DropDown from './photographers-page/DropDown.js';
 import Works from './photographers-page/Works.js';
-import Form from './photographers-page/Form.js';
+import Form from './photographers-page/form.js';
 
 // PHOTOGRAPHER PROFIL HEADER
 (new PhotographerProfil()).displayPhotographerProfil();
 
 // PHOTOGRAPHER GALLERY
 (new Works()).photographersWorks();
+
+// PHOTOGRAPHER'S WORK LIKES FUNCTION
+(new Works()).incrementLikes();
 
 // DROPDOWN MENU
 (new DropDown()).dropDown();
--- js/photographers-page/Works.js
+++ js/photographers-page/Works.js
@@ -21,9 +21,14 @@ export default class Works {
             <div class="ph-work-elt-text">
                 <h2 class="ph-work-title">${element.photoName}</h2>
                 <span class="ph-work-price">${element.price} €</span>
-                <span class="ph-work-like">${element.likes}
-                <i class="fas fa-heart" aria-label='likes' role="button"></i>
+                <div class='ph-elt-like'>
+                <span id="ph-work-like">
+                    <a class="like-counter" data-value="${element.likes}">${element.likes}</a>
                 </span>
+                <button class="btn-like" type="button">
+                    <i class="fas fa-heart" aria-label='likes' role="button"></i>
+                </button>
+                </div>
             </div>
                 `
                 articlePhWork.innerHTML = workTemplate;
@@ -43,5 +48,33 @@ export default class Works {
         <span># / jour</span>
         `
         box.innerHTML = boxTemplate;
+    }
+
+    async incrementLikes() {
+        await this.photographersWorks();
+        const heartBtnLike = document.getElementsByClassName('btn-like');
+        const counter = document.querySelector('.like-counter');
+
+        for (let i = 0; i < heartBtnLike.length; i++) {
+            let likes = 88;
+            let isLike = false;
+
+            if (heartBtnLike) {
+                heartBtnLike[i].addEventListener('click', () => {
+
+                    if (!isLike) {
+                        likes++;
+                        counter.innerHTML = likes;
+                        isLike = true;
+                    } else {
+                        likes--;
+                        counter.innerHTML = likes;
+                        isLike = false;
+                    }
+                })
+            } else {
+                console.log('erreur');
+            }
+        }
     }
 }
